0709061136a0791e354bc54f63dd61de1b9dae54
Claudio Cabello <claudio.cabello.v@mail.pucv.cl>
2023-10-29 16:32:13 -0300

    Cambio velocidad en powerup velocidad tarro y comentarios en clases varias

--- Game/core/src/com/mygdx/game/AumentoVelocidadLluviaPowerDown.java
+++ Game/core/src/com/mygdx/game/AumentoVelocidadLluviaPowerDown.java
@@ -5,26 +5,31 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AumentoVelocidadLluviaPowerDown implements PowerUp {
-	private Texture textura;
-	
-	public AumentoVelocidadLluviaPowerDown () {
-        this.textura = new Texture(Gdx.files.internal("velocidadlluvia.png"));
-    } 
-	
-	@Override
-	public void aplicarPowerUp(Tarro tarro,Lluvia lluvia) {
-		lluvia.setVelocidad(2);
-	}
+    private Texture textura;
 
-	public void quitarPowerUp(Tarro tarro,Lluvia lluvia) {
-		lluvia.setVelocidad(0.5);
-	}
-	
-	public void dibujar(SpriteBatch batch, float x, float y) {
+    // Constructor para cargar la textura del power-up
+    public AumentoVelocidadLluviaPowerDown() {
+        this.textura = new Texture(Gdx.files.internal("velocidadlluvia.png"));
+    }
+
+    // Método para aplicar el power-up y aumentar la velocidad de la lluvia
+    @Override
+    public void aplicarPowerUp(Tarro tarro, Lluvia lluvia) {
+        lluvia.setVelocidad(2);
+    }
+
+    // Método para quitar el power-up y restaurar la velocidad de la lluvia
+    public void quitarPowerUp(Tarro tarro, Lluvia lluvia) {
+        lluvia.setVelocidad(0.5);
+    }
+
+    // Método para dibujar el power-up en pantalla
+    public void dibujar(SpriteBatch batch, float x, float y) {
         batch.draw(textura, x, y);
     }
-	
-	public void destruir() {
-		textura.dispose();
-	}
+
+    // Método para liberar los recursos de la textura cuando ya no se necesite
+    public void destruir() {
+        textura.dispose();
+    }
 }
--- Game/core/src/com/mygdx/game/AumentoVelocidadPowerUp.java
+++ Game/core/src/com/mygdx/game/AumentoVelocidadPowerUp.java
@@ -4,27 +4,53 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Clase que implementa la interfaz PowerUp y representa un power-up
+ * que aumenta la velocidad de un objeto Tarro en un juego de lluvia.
+ */
 public class AumentoVelocidadPowerUp implements PowerUp {
-	private Texture textura;
-	
-	public AumentoVelocidadPowerUp () {
+    private Texture textura;
+    
+    /**
+     * Constructor de la clase que carga la textura del power-up.
+     */
+    public AumentoVelocidadPowerUp() {
         this.textura = new Texture(Gdx.files.internal("velocidad.png"));
     } 
-	
-	@Override
-	public void aplicarPowerUp(Tarro tarro,Lluvia lluvia) {
-		tarro.setVelocidad(1.5);
-	}
+    
+    /**
+     * Aplica el power-up al objeto Tarro, aumentando su velocidad.
+     * @param tarro El objeto Tarro al que se aplica el power-up.
+     * @param lluvia El objeto Lluvia en el juego.
+     */
+    @Override
+    public void aplicarPowerUp(Tarro tarro, Lluvia lluvia) {
+        tarro.setVelocidad(1.6);
+    }
 
-	public void quitarPowerUp(Tarro tarro,Lluvia lluvia) {
-		tarro.setVelocidad(0.6666666);
-	}
-	
-	public void dibujar(SpriteBatch batch, float x, float y) {
+    /**
+     * Quita el efecto del power-up, restaurando la velocidad normal del Tarro.
+     * @param tarro El objeto Tarro al que se le quita el power-up.
+     * @param lluvia El objeto Lluvia en el juego.
+     */
+    public void quitarPowerUp(Tarro tarro, Lluvia lluvia) {
+        tarro.setVelocidad(0.625);
+    }
+    
+    /**
+     * Dibuja la textura del power-up en un objeto SpriteBatch en las coordenadas (x, y).
+     * @param batch El objeto SpriteBatch en el que se dibuja.
+     * @param x La coordenada X donde se dibuja el power-up.
+     * @param y La coordenada Y donde se dibuja el power-up.
+     */
+    public void dibujar(SpriteBatch batch, float x, float y) {
         batch.draw(textura, x, y);
     }
-	
-	public void destruir() {
-		textura.dispose();
-	}
+    
+    /**
+     * Libera los recursos utilizados por la textura del power-up.
+     */
+    public void destruir() {
+        textura.dispose();
+    }
 }
