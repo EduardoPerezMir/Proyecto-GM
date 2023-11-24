803775ef2d33a05d6a68cbe8d7bc045f19b21282
Claudio Cabello <claudio.cabello.v@mail.pucv.cl>
2023-11-24 18:24:15 -0300

    Implementación de cambio en la creación de objetos por nivel

--- Game/core/src/com/mygdx/game/ObjetosNivelTres.java
+++ Game/core/src/com/mygdx/game/ObjetosNivelTres.java
@@ -4,19 +4,19 @@ import com.badlogic.gdx.math.MathUtils;
 
 public class ObjetosNivelTres implements ObjetosFactory {
 	public GotaBuena crearGotaBuena(int velInicialLluvia) {
-		return new GotaMorada(velInicialLluvia);
+		return new GotaAzul(velInicialLluvia);
 	}
 	
 	public GotaMala crearGotaMala(int velInicialLluvia) {
-		return new GotaRoja(velInicialLluvia);
+		return new GotaMortal(velInicialLluvia);
 	}
 	
 	public PowerUp crearPowerUp() {
-		return new InmortalidadPowerUp();
+		return new DuplicarPuntosPowerUp();
 	}
 	
 	public PowerDown crearPowerDown() {
-		return new InvertirPuntosPowerDown();
+		return new CongelarTarroPowerDown();
 	}
 	
 	public Tarro crearTarro() {
